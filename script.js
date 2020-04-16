daacb07377f75c3edc9109d9cb0c5ed54635e11e
amatyan <63625733+amatyan@users.noreply.github.com>
2020-04-16 18:46:43 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 $(function(){
 
-  $('.img-icon').on('click',function(){
+  $('.menu-icon').on('click',function(){
     $('#menu-modal').fadeIn();
+    $('.meun-icon').fadeOut();
+  });
+
+  $('.close-img').on('click',function(){
+    $('#menu-modal').fadeOut();
+    $('.menu-icon').fadeIn();
   });
 
 });
